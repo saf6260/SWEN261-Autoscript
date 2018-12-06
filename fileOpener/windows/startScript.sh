b2bca3780ce61c3a8a9e5753227a78d26eab0fac
Scott <saf6260@rit.edu>
2018-12-06 12:26:28 -0500

    File opener for SWEN-261 Testing sessions completed. There is both a version for linux and a version for windows

---
+++ fileOpener/windows/startScript.sh
@@ -0,0 +1,45 @@
+#System for opening all files from a .zip package for SWEN-261 in class testing purposes
+#Instructions can be found below or on github.com/saf6260/SWEN261-AutoScript
+#OS: WINDOWS
+#Author Scott Frauenknecht
+echo "Bash Script starting for file control. Please standby."
+
+#Unzipping the .zip extension from current directory
+echo "Unzipping file"
+for file in *.zip
+do
+  if [ -e "$file" ] 
+  then
+    unzip -d webcheckers "$file" 
+  else
+    clear
+    echo "No .zip files in current directory"
+    echo "Please move the .zip file into this directory"
+    exit 1
+  fi
+done
+
+#compiling project after unzip
+echo "unzip complete. Moving into file directory and compiling project"
+cd webcheckers
+mvn compile
+
+#opening tabs for game
+start chrome http://localhost:4567/
+start chrome --incognito http://localhost:4567/
+
+#Opening the excel file for the testers
+echo "opening excel file for use by testers"
+cd etc/
+start excel 'Acceptance Test Plan.xlsx'
+cd ..
+
+echo "App has compiled. Opening web pages."
+echo "Please give the webpages one moment to refresh while project runs"
+
+#executing the program
+mvn exec:java
+
+#when the testers end the exec with ^C, the terminal is cleared and displays a thank you message
+clear
+echo "Script complete. Thank you for testing!"
